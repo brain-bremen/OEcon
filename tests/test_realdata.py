dc771f3bb5d9c18596f7ddc8ac74a0bfbfef39e2
Joscha Schmiedt <schmiedt@uni-bremen.de>
2025-04-01 17:24:23 +0200

    WIP: Experiment with open-ephys-python-tools

--- tests/test_realdata.py
+++ tests/test_realdata.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 from open_ephys.analysis import Session
+from open_ephys.analysis.formats.BinaryRecording import BinaryRecording, Continuous
+from open_ephys.analysis.recording import ContinuousMetadata
+
 import pytest
 
 session_name = "Kobi_2025-03-31_11-48-13"
@@ -8,10 +11,25 @@ session_name = "Kobi_2025-03-31_11-48-13"
 data_folder = Path(__file__).parent / "data" / session_name
 
 # Check if the data folder exists
-skip_tests = data_folder.exists() and data_folder.is_dir()
+skip_tests = not (data_folder.exists() and data_folder.is_dir())
+
+# map stream_name to start CONT block id
+contstart = {"PXIe-6341": 1601, "example_data": 1}
 
 
 @pytest.mark.skipif(skip_tests, reason="Data folder does not exist")
 def test_load_data():
-    session = Session(data_folder)
+    session = Session(str(data_folder))
     assert session is not None, "Failed to load session data"
+
+    recording: BinaryRecording = session.recordnodes[0].recordings[0]
+    for cont in recording.continuous:
+        cont :Continuous
+        metadata : ContinuousMetadata = cont.metadata
+        start_cont_id = contstart[metadata.stream_name]
+
+        for index, name in enumerate(cont.metadata.channel_names):
+            id = start_cont_id + index
+            shape = cont.samples.shape
+
+    # assert recording is not None, "Failed to load recording data"
