00863ddb3ecffef7caa9a35a2df17f852d879df6
Joscha Schmiedt <schmiedt@uni-bremen.de>
2025-07-02 12:17:45 +0200

    Add function for scaling float data to 16-bit range

---
+++ src/oecon/scaling.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def scale_to_16_bit_range(
+    data: np.ndarray, scale_abs_max_to: int = 32765
+) -> tuple[np.ndarray, float]:
+    if scale_abs_max_to <= 0 or scale_abs_max_to > 2**16 / 2:
+        raise ValueError(
+            f"Integer value to be used for scaling the maximum data value to must be within the 16-bit range (0-{2**16 / 2})"
+        )
+
+    max_abs_value_in_data = np.max(np.abs(data))
+    factor: float = scale_abs_max_to / max_abs_value_in_data
+
+    scaled_data = (data * factor).astype(np.int16)
+    return scaled_data, factor
---
+++ tests/test_scaling.py
@@ -0,0 +1,28 @@
+from oecon.scaling import scale_to_16_bit_range
+import numpy as np
+import pytest
+
+
+def test_scale_to_16_bit_range():
+    # Test with a simple array
+    data = np.random.rand(100) * 37
+    scaled_data, factor = scale_to_16_bit_range(data, scale_abs_max_to=32765)
+
+    assert isinstance(scaled_data, np.ndarray)
+    assert scaled_data.dtype == np.int16
+    assert np.all(scaled_data >= -32768) and np.all(scaled_data <= 32767)
+
+    # Check if the maximum absolute value is scaled correctly
+    expected_scaled_data = np.int16(data * factor)
+    assert np.array_equal(scaled_data, expected_scaled_data)
+
+
+def test_scale_to_16_bit_range_errors():
+    # Test with an invalid scale_abs_max_to value
+    data = np.random.rand(100) * 37
+    with pytest.raises(ValueError) as exc_info:
+        scale_to_16_bit_range(data, scale_abs_max_to=40000)
+
+    # Test with a negative scale_abs_max_to value
+    with pytest.raises(ValueError) as exc_info:
+        scale_to_16_bit_range(data, scale_abs_max_to=-10)
